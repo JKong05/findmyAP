60e9d50014c2ae200af0198235888be423abdda6
JKong05 <jkong3662@gmail.com>
2024-05-31 04:36:54 -0400

    transferring code to mac

--- main.py
+++ main.py
@@ -6,3 +6,27 @@ def main():
 def get_input():
     while True:
         try:
+            n = int(input("How many AP exams have you taken? "))
+            if n == 0 or n > 38:
+                print("Null")
+                exit()
+            if n < 0:
+                print("Please enter a positive integer.")
+                continue
+            return n
+        except ValueError:
+            print("Please enter a valid integer.")
+            continue
+        except EOFError:
+            exit()
+
+def ap_list(n, data):
+    for exam in range(n):
+        course = input("Enter course: ")
+        score = input("Enter score received: ")
+        data[course] = score
+
+    return data
+
+if __name__ == "__main__":
+    main()
