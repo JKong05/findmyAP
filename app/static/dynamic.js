2a26c284e730bec4f61be49c538101d7e2d5176a
JKong05 <jkong3662@gmail.com>
2024-06-12 12:47:11 -0400

    Addressing mobile formatting concerns

--- app/static/dynamic.js
+++ app/static/dynamic.js
@@ -1,11 +1,24 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var typed = new Typed('.dynamic-title', {
-        strings: [
+    var strings;
+
+    if (window.innerWidth <= 786) {
+        strings = [
+            "save <span style='color: #3B71CA;'>time</span>",
+            "save <span style='color: #3B71CA;'>money</span>",
+            "save <span style='color: #3B71CA;'>stress</span>",
+            "findmy<br><span style='color: #24ace3'>A</span><span style='color: #97ca4f'>P</span>"
+        ]
+    } else {
+        strings = [
             "save <span style='color: #3B71CA;'>time</span>",
             "save <span style='color: #3B71CA;'>money</span>",
             "save <span style='color: #3B71CA;'>stress</span>",
             "findmy<span style='color: #24ace3'>A</span><span style='color: #97ca4f'>P</span>"
-        ],
+        ];
+    }
+
+    var typed = new Typed('.dynamic-title', {
+        strings: strings,
         typeSpeed: 150,
         backSpeed: 150,
         onComplete: function() {
